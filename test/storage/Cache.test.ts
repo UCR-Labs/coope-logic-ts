b6bdabd9f7733e967f3ae031bd2fd0d728bdde9f
Cristian Martínez <crismh2@hotmail.com>
2024-05-28 17:17:24 -0600

    Initial Commit

--- test/storage/Cache.test.ts
+++ test/storage/Cache.test.ts
@@ -9,41 +9,33 @@ describe("Cache", function () {
   });
 
   describe("SetKeyValue", function () {
-    it("should save a key-value pair", function (done) {
-      cache.SetKeyValue("key1", "value1", (err) => {
-        expect(err).to.be.null;
-        expect(cache["cache"]["key1"]).to.equal("value1");
-        done();
-      });
+    it("should save a key-value pair", async function () {
+      await cache.SetKeyValue("key1", "value1");
+      expect(cache["cache"]["key1"]).to.equal("value1");
     });
 
-    it("should return an error for invalid key or value", function (done) {
-      cache.SetKeyValue("", "value1", (err) => {
+    it("should return an error for invalid key or value", async function () {
+      try {
+        await cache.SetKeyValue("", "value1");
+      } catch (err) {
         expect(err).to.be.an.instanceOf(Error);
-        done();
-      });
+      }
     });
   });
 
   describe("GetKeyValue", function () {
-    beforeEach(function () {
-      cache["cache"]["key1"] = "value1";
+    beforeEach(async function () {
+      await cache.SetKeyValue("key1", "value1");
     });
 
-    it("should retrieve the value for an existing key", function (done) {
-      cache.GetKeyValue("key1", (err, value) => {
-        expect(err).to.be.null;
-        expect(value).to.equal("value1");
-        done();
-      });
+    it("should retrieve the value for an existing key", async function () {
+      const value = await cache.GetKeyValue("key1");
+      expect(value).to.equal("value1");
     });
 
-    it("should return null for a non-existing key", function (done) {
-      cache.GetKeyValue("nonExistingKey", (err, value) => {
-        expect(err).to.be.null;
-        expect(value).to.be.null;
-        done();
-      });
+    it("should return null for a non-existing key", async function () {
+      const value = await cache.GetKeyValue("nonExistingKey");
+      expect(value).to.be.null;
     });
   });
 });
