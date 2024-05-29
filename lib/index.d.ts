fc4551e115bea6ddceee39f960951a509439c32c
Cristian Martínez <crismh2@hotmail.com>
2024-05-29 07:30:04 -0600

    version 1.0.0

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,2 @@
 export { cacheSetKeyValue, cacheGetKeyValue } from "./src/storage/Cache";
 export { localStorageSetKeyValue, localStorageGetKeyValue, } from "./src/storage/LocalStorage";
-export { sqliteSetKeyValue, sqliteGetKeyValue } from "./src/storage/SQLite";
