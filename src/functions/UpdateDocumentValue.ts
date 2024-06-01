1650140d95333881e232e36e743a8ba6693cc65c
Andy Alvarado <130952193+Andy-Alvarado@users.noreply.github.com>
2024-06-01 15:38:34 -0600

    Update UpdateDocumentValue.ts

--- src/functions/UpdateDocumentValue.ts
+++ src/functions/UpdateDocumentValue.ts
@@ -1,27 +1,32 @@
-import { Firestore, DocumentData } from "firebase/firestore";
+import { Injectable } from '@angular/core';
+import { Firestore, DocumentData, PartialWithFieldValue, doc, getDoc, setDoc } from '@angular/fire/firestore';
+import { FirestoreCollections } from './models';
 
-export function updateDocumentValue(
-  firestore: Firestore,
-  doc: (firestore: Firestore, collection: string, docId: string) => any,
-  getDoc: (docRef: any) => Promise<any>,
-  setDoc: (docRef: any, data: Partial<DocumentData>, options: { merge: boolean }) => Promise<void>,
-  docId: string,
-  data: Partial<DocumentData>,
-  collection: string
-): Promise<void> {
-  return new Promise<void>(async (resolve, reject) => {
-    try {
-      const orderSettingDoc = doc(firestore, collection, docId);
-      const orderSettingsSnapshot = await getDoc(orderSettingDoc);
-  
-      if (orderSettingsSnapshot.exists()) {
-        await setDoc(orderSettingDoc, data, { merge: true });
-        resolve();
-      } else {
-        reject(new Error('Document does not exist.'));
+@Injectable({
+  providedIn: 'root'
+})
+export class UpdateDocumentValue {
+  constructor(private firestore: Firestore) {}
+
+  public updateDocumentValue(
+    docId: string,
+    data: PartialWithFieldValue<DocumentData>,
+    collection: keyof typeof FirestoreCollections
+  ): Promise<void> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const documentRef = doc(this.firestore, FirestoreCollections[collection], docId);
+        const documentSnapshot = await getDoc(documentRef);
+
+        if (documentSnapshot.exists()) {
+          await setDoc(documentRef, data, { merge: true });
+          resolve();
+        } else {
+          reject(new Error('Document does not exist.'));
+        }
+      } catch (error) {
+        reject(error);
       }
-    } catch (error) {
-      reject(error);
-    }
-  });
+    });
+  }
 }
