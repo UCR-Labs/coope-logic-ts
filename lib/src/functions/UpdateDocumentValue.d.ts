e9658fb39b51c9e7d0d35a637b649fe2721da70b
Dylan Tenorio Rojas <dylantr2001@gmail.com>
2024-06-02 16:35:51 -0600

    fixing bugs

--- lib/src/functions/UpdateDocumentValue.d.ts
+++ lib/src/functions/UpdateDocumentValue.d.ts
@@ -1,7 +1,3 @@
 import { Firestore, DocumentData, PartialWithFieldValue } from '@angular/fire/firestore';
 import { FirestoreCollections } from 'CoopeTypes';
-export declare class UpdateDocumentValue {
-    private firestore;
-    constructor(firestore: Firestore);
-    updateDocumentValue(docId: string, data: PartialWithFieldValue<DocumentData>, collection: keyof typeof FirestoreCollections): Promise<void>;
-}
+export declare function updateDocumentValue(firestore: Firestore, docId: string, data: PartialWithFieldValue<DocumentData>, collection: keyof typeof FirestoreCollections): Promise<void>;
