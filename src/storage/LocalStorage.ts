f7e7995c3a0e0bf584966a75616867a5cf9e63cd
Andy Alvarado <130952193+Andy-Alvarado@users.noreply.github.com>
2024-06-02 13:41:19 -0600

    Add functions to the LocalStorage class and updating the set and get methods

--- src/storage/LocalStorage.ts
+++ src/storage/LocalStorage.ts
@@ -1,3 +1,5 @@
+import {encrypt, decrypt} from "../functions/Encryption"
+
 export function localStorageSetKeyValue(
   key: string,
   value: any
@@ -7,7 +9,26 @@ export function localStorageSetKeyValue(
       return reject(new Error("Invalid key or value"));
     }
     try {
-      localStorage.setItem(key, JSON.stringify(value));
+      const encryptValue = encrypt(JSON.stringify(value), key);
+      localStorage.setItem(key, encryptValue);
+      resolve();
+    } catch (err) {
+      reject(err);
+    }
+  });
+}
+
+export function localStorageSetKeyValueAsString(
+  key: string,
+  value: any
+): Promise<void> {
+  return new Promise((resolve, reject) => {
+    if (!key || value === undefined) {
+      return reject(new Error("Invalid key or value"));
+    }
+    try {
+      const encryptalue = encrypt(String(value), key);
+      localStorage.setItem(key, encryptalue);
       resolve();
     } catch (err) {
       reject(err);
@@ -19,17 +40,35 @@ export function localStorageGetKeyValue(key: string): Promise<any> {
   return new Promise((resolve, reject) => {
     try {
       const item = localStorage.getItem(key);
-      const value = item !== null ? JSON.parse(item) : null;
-      resolve(value);
+      if (item !== null) {
+        const decryptedValue = decrypt(item, key);
+        resolve(JSON.parse(decryptedValue));
+      } else {
+        resolve(null);
+      }
     } catch (err) {
       reject(err);
     }
   });
 }
 
+export function localStorageGetKeyValueWithoutPromise(key: string): any {
+  try {
+    const item = localStorage.getItem(key);
+    if (item !== null) {
+      const decryptedValue = decrypt(item, key);
+      return JSON.parse(decryptedValue);
+    }
+  } catch (err) {
+    console.error("Error parsing JSON from localStorage", err);
+    return null;
+  }
+}
+
+
 export function localStorageKeyExists(key: string): Promise<boolean> {
   return new Promise((resolve) => {
     const item = localStorage.getItem(key);
-    resolve(item !== null);
-  });
+    resolve(item !== null);
+  });
 }
