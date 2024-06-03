664eff6a4f981db3b2d15554107b51002ebdc772
Andy Alvarado <130952193+Andy-Alvarado@users.noreply.github.com>
2024-06-02 20:01:41 -0600

    delete export for pushNotifications

--- index.ts
+++ index.ts
@@ -14,5 +14,4 @@ export {
 } from "./src/functions/Average";
 export { encrypt, decrypt } from "./src/functions/Encryption";
 export { geocode } from "./src/functions/GoogleMaps";
-export { adminPushNotifications } from "./src/functions/PushNotification";
 export { updateDocumentValue }from "./src/functions/UpdateDocumentValue";
