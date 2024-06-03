288f27f3636ac38790901f90d01b7fe33fdba6d8
DylanGabrielRojas <36280318+DylanGabrielRojas@users.noreply.github.com>
2024-06-02 22:15:52 -0600

    Revert "Add PushNotifications back"

--- lib/src/functions/PushNotification.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Obtains a document with the notification to send and performs the necessary logic
- * to send that notification to the corresponding devices
- */
-export declare function adminPushNotifications(title: string, body: string, tokens: string[]): Promise<void>;
