f50f8f5d22a33ed404e971ae37427d1603f4a4c3
Jafet Picado <86694602+Jafet-PicadoQ@users.noreply.github.com>
2024-06-04 18:42:28 -0600

    Se cambian nombres de parametros

--- src/functions/Encryption.ts
+++ src/functions/Encryption.ts
@@ -4,7 +4,7 @@ export function encrypt(text: string, key: string): string {
     return CryptoJS.AES.encrypt(text, key).toString();
 }
 
-export function decrypt(ciphertext: string, key: string): string {
-    const bytes = CryptoJS.AES.decrypt(ciphertext, key);
+export function decrypt(text: string, key: string): string {
+    const bytes = CryptoJS.AES.decrypt(text, key);
     return bytes.toString(CryptoJS.enc.Utf8);
 }
