fd12fb7a3dfc6b31b71c1a906b8ea27fa9e364cd
Jafet Picado <86694602+Jafet-PicadoQ@users.noreply.github.com>
2024-06-04 18:34:17 -0600

    Se cambia la implementacion de encryption

--- lib/src/functions/Encryption.d.ts
+++ lib/src/functions/Encryption.d.ts
@@ -1,2 +1,2 @@
-export declare function encrypt(text: string, key: string): string;
-export declare function decrypt(text: string, key: string): string;
+export declare function encrypt(text: string, secretKey: string): string;
+export declare function decrypt(ciphertext: string, secretKey: string): string;
