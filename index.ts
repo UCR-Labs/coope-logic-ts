ac7e895b5f4d988a6c661cc90a8eb260fad46e91
Josué R.R <73318183+JosueRR@users.noreply.github.com>
2024-06-22 18:33:46 -0600

    Elimina funciones que no se usan

--- index.ts
+++ index.ts
@@ -1,46 +1,8 @@
 export {
-    cacheSetKeyValue,
-    cacheGetKeyValue,
-    cacheKeyExists,
-} from "./src/storage/Cache";
-export {
     localStorageSetKeyValue,
     localStorageGetKeyValue,
     localStorageKeyExists,
     localStorageGetKeyValueWithoutPromise,
     localStorageSetKeyValueAsString,
 } from "./src/storage/LocalStorage";
-export {
-    calculateAverageRatingForUser,
-    getAverageRatings,
-} from "./src/functions/Average";
 export { encrypt, decrypt } from "./src/functions/Encryption";
-export { geocode } from "./src/functions/GoogleMaps";
-export { DirectionsAPI } from "./src/functions/Endpoints";
-export { updateDocumentValue } from "./src/functions/UpdateDocumentValue";
-export {
-    updateOrder,
-    getUserFCMToken,
-    getBikerName,
-    sendMessagePending,
-    sendMessageAcceptedBiker,
-    sendMessageArriving,
-    sendMessageRating,
-} from "./src/functions/Orders_management";
-export {
-    handleUserFcmTokenUpdate,
-    updateFCMToken,
-} from "./src/functions/Fcm_management";
-export {
-    getProducts,
-    fetchProductsFromFirebase,
-} from "./src/functions/Products";
-export {
-    getBusinesses,
-    fetchBusinessesFromFirebase,
-    getBusinessById,
-    fetchBusinessFromFirebase,
-} from "./src/functions/Businesses";
-export { calculateServiceCost } from "./src/orders/serviceCost";
-export { rating } from "./src/ratings/ratings_management";
-export { getAverageRating } from "./src/ratings/average_rating_management";
